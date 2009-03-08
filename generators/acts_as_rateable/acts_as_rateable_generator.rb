5f78774416427beb87dcecd195f2f0baa8d26e08
Roman Scherer <roman.scherer@burningswell.com>
2009-03-08 15:27:01 +0100

    Fixed migration generator.

--- generators/acts_as_rateable/acts_as_rateable_generator.rb
+++ generators/acts_as_rateable/acts_as_rateable_generator.rb
@@ -1,11 +1,13 @@
 class ActsAsRateableGenerator < Rails::Generator::Base
 
+  attr_reader :migration_name
+
   def manifest
 
     @migration_name = "CreateRatings"
 
     record do |m|
-      m.migration_template 'acts_as_rateable_migration.rb', File.join("db", "migrate"), { :migration_file_name => "create_ratings" }
+      m.migration_template 'acts_as_rateable_migration.rb.erb', File.join("db", "migrate"), { :migration_file_name => "create_ratings" }
     end
 
   end

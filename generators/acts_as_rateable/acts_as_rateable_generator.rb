1b563225f18a45d60fc5b4ae98fd6d5180e96e68
Roman Scherer <roman.scherer@burningswell.com>
2009-03-08 15:23:40 +0100

    Added the migration generator.

--- generators/acts_as_rateable/acts_as_rateable_generator.rb
+++ generators/acts_as_rateable/acts_as_rateable_generator.rb
@@ -1,8 +1,13 @@
-class ActsAsRateableGenerator < Rails::Generator::NamedBase
+class ActsAsRateableGenerator < Rails::Generator::Base
+
   def manifest
+
+    @migration_name = "CreateRatings"
+
     record do |m|
-      # m.directory "lib"
-      # m.template 'README', "README"
+      m.migration_template 'acts_as_rateable_migration.rb', File.join("db", "migrate"), { :migration_file_name => "create_ratings" }
     end
+
   end
+
 end
